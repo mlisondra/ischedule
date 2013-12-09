beea491e3e783e490b31e3f9f3518fc26d394065
mlisondra <milder.lisondra@lisondraconsulting.com>
2013-12-08 21:39:50 -0800

    Change destination for AJAX calls
    AJAX calls were modified to user controller

--- js/init_calendar.js
+++ js/init_calendar.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 		  get_contacts(); // found in /js/schedule.js
 		  get_categories(); // found in /js/schedule.js
-		  //get_user_events();
+		  //get_user_events(); // found in /js/schedule.js
 		  
 		  //initialize the calendar
 		  
@@ -9,9 +9,9 @@ $(document).ready(function() {
 			    eventClick: function(calEvent, jsEvent, view) { //clicking on event within calendar brings up Edit Event dialog box
 					show_modal('edit_event',calEvent.id);
 				},
-				dayClick: function(date, allDay, jsEvent, view){ //clicking within a calendar day brings up the Add Event
-					show_modal('add_event');
-					var begin_date = $.fullCalendar.formatDate(date, 'MM/dd/yyyy');
+                            dayClick: function(date, allDay, jsEvent, view){ //clicking within a calendar day brings up the Add Event
+                                show_modal('add_event');
+                                var begin_date = $.fullCalendar.formatDate(date, 'MM/dd/yyyy');
 				
 					$.post('../schedule/process.php',{"action":"get_modal_content","modal_type":"add_event","begin_date":begin_date,"end_date":begin_date},
 						function(data){
@@ -41,15 +41,16 @@ $(document).ready(function() {
 					left: 'month,basicWeek,basicDay',
 					center: 'title',
 					right:'today prev,next'
-				}/*,
-				events: "../controller/events.php",
+				},
+				//events: "../controller/events.php",
+                                events : "/schedule/get_events",
 				eventRender: function(event,element){
 					$('.'+event.className).css("background-color",event.color);
 					$('.'+event.className).css("border-color",event.color);
 					$('.'+event.className).css("border-style","solid");
 					$('.'+event.className).css("color","#000");
 					$('.'+event.className).css("font-weight","bold");
-				}*/
+				}
 			})
 			
 			 $('.calendar_manager').live('keyup.autocomplete', 
