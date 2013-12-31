96233d45a0217c84241f0bf15d22c777e149f7be
mlisondra <milder.lisondra@lisondraconsulting.com>
2013-12-31 08:12:59 -0800

    Minor spacing removed

--- application/views/templates/add_category.php
+++ application/views/templates/add_category.php
@@ -1,5 +1,5 @@
 <div id="form_notification"></div><br/>
-<form name="add_category_form" id="add_category_form" method="post">
+<form name="add_category_form" id="add_category_form" method="post" class="iform">
 <table>
 <tr><td>Name</td></tr>
 <tr><td><input type="text" name="name" id="name" value=""></td></tr>
@@ -8,22 +8,21 @@
 </td></tr>
 <tr><td>Description</td></tr>
 <tr><td>
-        <textarea name="description" id="description" rows="5" cols="35"></textarea>
+        <textarea name="description" id="description"></textarea>
         <div style="margin-top:10px;">
-        <input type="button" class="save_button" value="Save">
+        <input type="submit" name="add_category_button" value="Add" class="save_button" rel="add_category">
         <input type="button" class="save_button_cancel" value="Cancel"></div>
         </div>
     </td></tr>
 <input type="hidden" name="action" value="add_category">
 </form>
-		<script type="text/javascript">
-			$(document).ready(function(){
-				$(".color-picker").miniColors({
-					letterCase: 'lowercase',
-					change: function(hex, rgb) {
-						//logData(hex, rgb);
-						$(".miniColors-selector").hide();
-					}
-				});
-			});
-		</script>
+<script type="text/javascript">
+        $(document).ready(function(){
+                $(".color-picker").miniColors({
+                        letterCase: 'lowercase',
+                        change: function(hex, rgb) {
+                                $(".miniColors-selector").hide();
+                        }
+                });
+        });
+</script>
