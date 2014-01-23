200f66f0930309092e671ab0c99cc2c3b4bb172d
mlisondra <milder.lisondra@lisondraconsulting.com>
2014-01-22 19:15:14 -0800

    Modify form
    Added class attribute to form tag; foreach added for phone_carrier_list;
    calendars added.

--- application/views/templates/add_contact.php
+++ application/views/templates/add_contact.php
@@ -1,14 +1,23 @@
 <div id="form_notification"></div><br/>
-<form name="add_contact_form" id="add_contact_form" method="post">
+<form name="add_contact_form" id="add_contact_form" method="post" class="iform">
 <table>
 <tr><td>First Name</td><td><input type="text" name="first_name" id="first_name" value="" size="30"></td></tr>
 <tr><td>Last Name</td><td><input type="text" name="last_name" id="last_name" value="" size="30"></td></tr>
 <tr><td>Email</td><td><input type="text" name="email" id="email" value="" size="30"></td></tr>
 <tr><td>Phone</td><td><input type="text" name="phone" id="phone" value=""></td></tr>
 <tr><td>Phone Carrier&nbsp;</td>
-<td>##phone_carrier_list##</td></tr>
+<td>
+    <?php 
+    echo '<select name="phone_carrier" id="phone_carrier">
+	<option value="" selected>--Select One--</option>';
+    foreach($phone_carriers as $carrier){
+        echo '<option>'. $carrier . '</option>';
+    } 
+    echo '</select>';
+    ?>
+</td></tr>
 <tr valign="top"><td>Calendar</td><td>
-##user_categories##
+<?php print $calendars; ?>
 </td></tr>
 <tr>
 	<td>Reminder Type</td>
@@ -22,5 +31,6 @@
 	</td>
 </tr>
 </table>
-<input type="hidden" name="action" value="add_contact">
+<input type="submit" name="add_contact_button" value="Add" class="save_button" rel="add_contact">
+<input type="button" class="save_button_cancel" value="Cancel">
 </form>
