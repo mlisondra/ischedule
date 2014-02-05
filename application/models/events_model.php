7db34d77c37a22b4cb71d87ad991a350773e475f
mlisondra <milder.lisondra@lisondraconsulting.com>
2014-02-04 19:44:01 -0800

    New model for Events

---
+++ application/models/events_model.php
@@ -0,0 +1,33 @@
+<?php
+
+class Events_model extends CI_Model {
+
+	//protected $accounts = 'users';
+	//protected $calendars = 'user_categories';
+        //protected $calendar_managers = 'calendar_managers';
+        protected $events = 'user_events';
+        //protected $contacts = 'user_contacts';
+        protected $cdt = "";
+
+        public function __construct(){
+            parent::__construct();
+            $this->cdt = mktime();
+        }
+        
+        /**
+         * Get a single event or multiple events
+         * @param int $id Event id
+         */
+        public function get($id){
+            //print_r($id);
+            $this->db->where('id',$id);
+            $query = $this->db->get($this->events);
+		if($query->num_rows() > 0){
+			return $query->row();
+		}else{
+			return 0;
+		}            
+            //print($this->db->last_query());
+        }
+        
+}
